2a9e70168867a75d0f16e8e65d263ffb55f52e86
sano-suguru <sa.50.00.no.riku+github@gmail.com>
2018-10-27 11:12:11 +0900

    [update]Bookモデルを実装

--- Book.cs
+++ Book.cs
@@ -1,8 +1,13 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Text;
-
-namespace readonly_model_props {
+﻿namespace readonly_model_props {
   class Book {
+    public int Id { get; private set; }
+    public string Title { get; private set; }
+    public string Author { get; private set; }
+
+    public Book(int id, string title, string author) {
+      Id = id;
+      Title = title;
+      Author = author;
+    }
   }
 }
