ea4d49b184e1d67cfddf878aa6032ffdc72e0d9b
sano-suguru <sa.50.00.no.riku+github@gmail.com>
2018-10-27 11:06:20 +0900

    プロジェクトを新規作成

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace readonly_model_props
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
