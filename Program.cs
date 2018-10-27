5cb9f7c85c17668a3eea3dd886578b8792352d6e
sano-suguru <sa.50.00.no.riku+github@gmail.com>
2018-10-27 11:41:56 +0900

    [update]抽出クエリを実装

--- Program.cs
+++ Program.cs
@@ -1,9 +1,19 @@
 ﻿using System;
+using System.Linq;
 
 namespace readonly_model_props {
   class Program {
     static void Main(string[] args) {
-      Console.WriteLine("Hello World!");
+      using (var context = new InMemoryDbContext()) {
+        context.Database.EnsureCreated();
+      }
+
+      using (var context = new InMemoryDbContext()) {
+        var books = context.Books.AsEnumerable();
+        foreach (var book in books) {
+          Console.WriteLine($"{book.Id} {book.Title} {book.Author}");
+        }
+      }
     }
   }
 }
