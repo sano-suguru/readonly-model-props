d7a4a2a9ae2eb5155f361c2c06e305c448bc62a6
sano-suguru <sa.50.00.no.riku+github@gmail.com>
2018-10-27 14:37:12 +0900

    [update]プロパティを内部的にも読み取り専用に変更

--- Book.cs
+++ Book.cs
@@ -1,8 +1,8 @@
 ﻿namespace readonly_model_props {
   class Book {
-    public int Id { get; private set; }
-    public string Title { get; private set; }
-    public string Author { get; private set; }
+    public int Id { get; }
+    public string Title { get; }
+    public string Author { get; }
 
     public Book(int id, string title, string author) {
       Id = id;
--- InMemoryDbContext.cs
+++ InMemoryDbContext.cs
@@ -7,11 +7,17 @@ namespace readonly_model_props {
     protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder) =>
       optionsBuilder.UseInMemoryDatabase(databaseName: "InMemoryDatabase");
 
-    protected override void OnModelCreating(ModelBuilder modelBuilder) =>
+    protected override void OnModelCreating(ModelBuilder modelBuilder) {
+      modelBuilder.Entity<Book>(book => {
+        book.HasKey(nameof(Book.Id));
+        book.Property(e => e.Title);
+        book.Property(e => e.Author);
+      });
       modelBuilder.Entity<Book>().HasData(
         new Book(id: 1, title: "Flutter入門", author: "掌田 津耶乃"),
         new Book(id: 2, title: "Nuxt.jsビギナーズガイド", author: "花谷 拓磨"),
         new Book(id: 3, title: "IntelliJ IDEAハンズオン", author: "山本 裕介")
       );
+    }
   }
 }
