be26d12314d1960c0197f365593d50eb0f63ff4b
sano-suguru <sa.50.00.no.riku+github@gmail.com>
2018-10-27 11:30:21 +0900

    [fix]フォーマットを修正

--- Program.cs
+++ Program.cs
@@ -1,12 +1,9 @@
 ﻿using System;
 
-namespace readonly_model_props
-{
-    class Program
-    {
-        static void Main(string[] args)
-        {
-            Console.WriteLine("Hello World!");
-        }
+namespace readonly_model_props {
+  class Program {
+    static void Main(string[] args) {
+      Console.WriteLine("Hello World!");
     }
+  }
 }
